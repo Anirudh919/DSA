9bb450a2178c656d088bbcbd2370cdb6034e6954
Anirudh Kashyap <anirudh.kashyap@oodles.io>
2025-06-24 18:43:28 +0530

    Update

--- practice/NaturalNum.java
+++ practice/NaturalNum.java
@@ -1,11 +1,19 @@
 package practice;
 
+import java.util.Scanner;
+
 public class NaturalNum {
     public static void main(String[] args) {
-        int n = 10, sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum = sum + i; // add i instead of 1
-        }
-        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+//        int n = 10, sum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            sum = sum + i; // add i instead of 1
+//        }
+//        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+        Scanner s = new Scanner(System.in);
+        int p= s.nextInt();
+        int r=s.nextInt();
+        int t=s.nextInt();
+        int result = (p*r*t)/100;
+        System.out.print("Rate of interest :"+result);
     }
 }
